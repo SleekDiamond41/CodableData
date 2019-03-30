a7c2641e01abcb61969562880daf99680adad4b5
Michael Arrington <michael@clevercoding.com>
2019-03-30 15:21:54 -0700

    Initial Commit

---
+++ SQL/SQL.h
@@ -0,0 +1,19 @@
+//
+//  SQL.h
+//  SQL
+//
+//  Created by Michael Arrington on 3/30/19.
+//  Copyright © 2019 Duct Ape Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SQL.
+FOUNDATION_EXPORT double SQLVersionNumber;
+
+//! Project version string for SQL.
+FOUNDATION_EXPORT const unsigned char SQLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SQL/PublicHeader.h>
+
+
---
+++ SQLTests/SQLTests.swift
@@ -0,0 +1,34 @@
+//
+//  SQLTests.swift
+//  SQLTests
+//
+//  Created by Michael Arrington on 3/30/19.
+//  Copyright © 2019 Duct Ape Productions. All rights reserved.
+//
+
+import XCTest
+@testable import SQL
+
+class SQLTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
