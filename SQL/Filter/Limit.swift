e57d3c70a0906952d0c65bd6176108f3e3de294d
Michael Arrington <michael@clevercoding.com>
2019-04-03 19:00:03 -0600

    Adding Limit file to git

---
+++ SQL/Filter/Limit.swift
@@ -0,0 +1,24 @@
+//
+//  Limit.swift
+//  SQL
+//
+//  Created by Michael Arrington on 4/3/19.
+//  Copyright © 2019 Duct Ape Productions. All rights reserved.
+//
+
+import Foundation
+
+
+public struct Limit {
+	var query: String {
+		return "LIMIT \(limit) OFFSET \(limit * (page-1))"
+	}
+	
+	public let limit: Int
+	public let page: Int
+	
+	public init(_ limit: Int, _ page: Int = 1) {
+		self.limit = limit
+		self.page = page
+	}
+}
